3a24aa0beef9b8c0f76a0772b9b1ccf0376a5d86
Victor Leandro Santos <victor@innvodigital.com>
2026-05-02 19:04:29 -0300

    creation structure project

---
+++ src/main/java/systemdesigns/study/SystemDesignsApplication.java
@@ -0,0 +1,13 @@
+package systemdesigns.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDesignsApplication {
+
+	 static void main(String[] args) {
+		SpringApplication.run(SystemDesignsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/systemdesigns/study/SystemDesignsApplicationTests.java
@@ -0,0 +1,13 @@
+package systemdesigns.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDesignsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
